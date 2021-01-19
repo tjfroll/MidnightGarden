425cff8659180262a3ff83b371e18cc2cfa36849
Tim Froehlich <tjfroll@gmail.com>
2021-01-19 02:50:27 -0500

    Init setup

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const PageRouter = () => {
+  return (
+    <>
+      <header>
+        <h1>midnight garden</h1>
+        <nav>
+          <a>one</a>
+          <a>two</a>
+          <a>three</a>
+        </nav>
+      </header>
+      <main>
+        <h2>current page</h2>
+      </main>
+      <footer>
+        <h3>footer</h3>
+      </footer>
+    </>
+  )
+}
+export default PageRouter
