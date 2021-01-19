8ce148f3d072bd324c05806293bd3ce148964177
Tim Froehlich <tjfroll@gmail.com>
2021-01-19 03:10:17 -0500

    Linter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+import React from 'react'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../global.css'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
